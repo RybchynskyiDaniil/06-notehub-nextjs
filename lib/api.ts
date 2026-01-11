061989e054a6cd9ab46b29c1e33c5dbc626b0245
Daniil Rybchynskyi <rybchynskyidaniil@gmail.com>
2026-01-11 13:38:43 +0000

    ff

---
+++ lib/api.ts
@@ -0,0 +1,48 @@
+import type { Note, NoteTag } from "../src/types/note";
+import axios from "axios";
+const API_URL = "https://notehub-public.goit.study/api/notes";
+const NOTEHUB_TOKEN = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN
+
+export interface NotesResponse {
+    notes: Note[];
+    totalPages:number
+} 
+
+export interface NewNoteData {
+    title: string;
+    content?: string;
+    tag:NoteTag
+}
+
+export async function fetchNotes(page?: number, search?: string): Promise<NotesResponse>{
+    const response = await axios.get<NotesResponse>(API_URL, {
+        params: {
+        page,
+        search,
+        },
+        headers: {
+            Authorization: `Bearer ${NOTEHUB_TOKEN}`,
+        }
+ })
+    return response.data
+}
+
+export async function createNote (newNote:NewNoteData):Promise<Note> {
+    const response = await axios.post<Note>(API_URL, newNote, {
+        headers: {
+            accept: "application/json",
+            Authorization: `Bearer ${NOTEHUB_TOKEN}`,
+        },
+    });
+    return response.data;
+}
+
+export async function deleteNote(noteID :string) {
+    const deletedNote = await axios.delete<Note>(`${API_URL}/${noteID}`, {
+        headers: {
+            accept: "application/json",
+            Authorization: `Bearer ${NOTEHUB_TOKEN}`,
+        },
+    });
+    return deletedNote.data;
+}
